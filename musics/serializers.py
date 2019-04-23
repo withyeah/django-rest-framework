c8b28bbbe4d82b45705c287a16467483a5938b2c
withyeah <areygim@gmail.com>
2019-04-23 06:35:46 +0000

    01_artist list && artist detail && comment create

--- musics/serializers.py
+++ musics/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import Music
+from .models import Music, Artist, Comment
 
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
         fields = ['id', 'title', 'artist',]
+        
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ['id', 'name',]
+        
+class ArtistDetailSerializer(serializers.ModelSerializer):
+    music_set = MusicSerializer(many=True)
+    class Meta:
+        model = Artist
+        fields = ['id', 'name', 'music_set',]
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content',]
+        
--- musics/views.py
+++ musics/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import MusicSerializer
-from .models import Music
+from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer
+from .models import Music, Artist
 # Create your views here.
 
 # Response 를 통해 Serializer 를 반환
@@ -19,3 +19,29 @@ def music_list(request):
     # 단일 객체가 아닐 때 many=True 를 꼭 써야 함
     serializer = MusicSerializer(musics, many=True)
     return Response(serializer.data)
+    
+@api_view(['GET'])
+def music_detail(request, music_pk):
+    music = get_object_or_404(Music, pk=music_pk)
+    serializer = MusicSerializer(music)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def artist_list(request):
+    artists = Artist.objects.all()
+    serializer = ArtistSerializer(artists, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def artist_detail(request, artist_pk):
+    artist = get_object_or_404(Artist, pk=artist_pk)
+    serializer = ArtistDetailSerializer(artist)
+    return Response(serializer.data)
+    
+@api_view(['POST'])
+def comment_create(request, music_pk):
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(music_id=music_pk)
+        return Response(serializer.data)
+    
